7178fe86064fc482e35dc064b9b51cf477589732
Nirvan Nagar <nirvan.nagar@gmail.com>
2022-11-09 22:53:22 -0500

    Created a new initializer for the Goal struct

--- Champion/Models/Goal.swift
+++ Champion/Models/Goal.swift
@@ -12,4 +12,15 @@ struct Goal: Identifiable, Hashable, Equatable, Codable {
     let scorer: Participant
     let against: Participant
     let minute: Int?
+    
+    init(scorer: Participant, against: Participant, minute: Int? = nil) {
+        self.init(id: IdUtils.newUuid, scorer: scorer, against: against, minute: minute)
+    }
+    
+    init(id: String, scorer: Participant, against: Participant, minute: Int?) {
+        self.id = id
+        self.scorer = scorer
+        self.against = against
+        self.minute = minute
+    }
 }
